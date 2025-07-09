65c8df0a07c5c6aa944c13b325eb18496ce59ea4
sandocsol <jungan1030@gmail.com>
2025-07-09 21:48:42 +0900

    chore: setup React project, add: albumArt

---
+++ week01/JUNGAN/playlist/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const songs = [
+    { id: 1, title: 'astroworld', artist: 'Artist 1', albumArt: '/albumArt/astroworld.jpeg' },
+    { id: 2, title: 'BEIGE', artist: 'Artist 2', albumArt: '/albumArt/BEIGE.jpeg' },
+    { id: 3, title: 'blonde', artist: 'Artist 3', albumArt: '/albumArt/blonde.jpeg' },
+    { id: 4, title: 'Deep End Mix Tape', artist: 'Artist 4', albumArt: '/albumArt/Deep End Mix Tape.jpeg' },
+    { id: 5, title: 'tellusaboutyourself', artist: 'Artist 5', albumArt: '/albumArt/tellusaboutyourself.jpeg' },
+  ]
+
+  return (
+    
+  )
+}
+
+export default App
