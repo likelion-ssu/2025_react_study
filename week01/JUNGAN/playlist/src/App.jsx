eb6ff708ca2160ccf3f08c9e7468ef9894749ff5
sandocsol <jungan1030@gmail.com>
2025-07-10 16:39:13 +0900

    feat: Song, App 컴포넌트 구현

--- week01/JUNGAN/playlist/src/App.jsx
+++ week01/JUNGAN/playlist/src/App.jsx
@@ -1,19 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+import styled from 'styled-components'
+import Song from './song.jsx'
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+`
 
 function App() {
   const songs = [
-    { id: 1, title: 'astroworld', artist: 'Artist 1', albumArt: '/albumArt/astroworld.jpeg' },
-    { id: 2, title: 'BEIGE', artist: 'Artist 2', albumArt: '/albumArt/BEIGE.jpeg' },
-    { id: 3, title: 'blonde', artist: 'Artist 3', albumArt: '/albumArt/blonde.jpeg' },
-    { id: 4, title: 'Deep End Mix Tape', artist: 'Artist 4', albumArt: '/albumArt/Deep End Mix Tape.jpeg' },
-    { id: 5, title: 'tellusaboutyourself', artist: 'Artist 5', albumArt: '/albumArt/tellusaboutyourself.jpeg' },
+    { id: 1, title: 'astroworld', artist: 'Travis Scott', albumArt: '/albumArt/astroworld.jpeg' },
+    { id: 2, title: 'BEIGE', artist: '키드밀리', albumArt: '/albumArt/BEIGE.jpeg' },
+    { id: 3, title: 'blonde', artist: 'Frank Ocean', albumArt: '/albumArt/blonde.jpeg' },
+    { id: 4, title: 'Deep End Mix Tape', artist: 'Tabber', albumArt: '/albumArt/Deep End Mix Tape.jpeg' },
+    { id: 5, title: 'tellusaboutyourself', artist: '백예린', albumArt: '/albumArt/tellusaboutyourself.jpeg' },
   ]
 
   return (
-    
+    <Wrapper>
+      {songs.map(song => (
+        <Song key={song.id} title={song.title} artist={song.artist} albumArt={song.albumArt} />
+      ))}
+    </Wrapper>
   )
 }
 
--- week01/JUNGAN/playlist/src/Song.jsx
+++ week01/JUNGAN/playlist/src/Song.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export default function song() {
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  height: 300px;
+  width: 300px;
+  border-radius: 15px;
+  background-color: #f7eeda;
+  color: #333;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+export default function song(props) {
+    const { title, artist, albumArt } = props
   return (
-    <div>song</div>
+    <Wrapper>
+        <img src={albumArt} alt={`${title} album art`} style={{ width: '100px', height: '100px' }} />
+        <h3>{title}</h3>
+        <p>{artist}</p>
+    </Wrapper>
   )
 }
