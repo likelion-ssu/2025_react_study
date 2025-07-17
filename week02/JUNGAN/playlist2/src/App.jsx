b0981bd27f17d78ce28c8a75613abd6e97fac415
sandocsol <jungan1030@gmail.com>
2025-07-18 08:07:07 +0900

    feat: next, prev 버튼, 좋아요 기능 완성

--- week02/JUNGAN/playlist2/src/App.jsx
+++ week02/JUNGAN/playlist2/src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import styled from 'styled-components'
 import Song from './Song.jsx'
@@ -11,6 +11,16 @@ const Wrapper = styled.div`
 `
 
 function App() {
+  const [index, setIndex] = useState(0);
+
+  const handleNext = () => {
+    setIndex((index + 1) % songs.length);
+  };
+
+  const handlePrev = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + songs.length) % songs.length);
+  };
+
   const songs = [
     { id: 1, title: 'astroworld', artist: 'Travis Scott', albumArt: '/albumArt/astroworld.jpeg' },
     { id: 2, title: 'BEIGE', artist: '키드밀리', albumArt: '/albumArt/BEIGE.jpeg' },
@@ -19,11 +29,25 @@ function App() {
     { id: 5, title: 'tellusaboutyourself', artist: '백예린', albumArt: '/albumArt/tellusaboutyourself.jpeg' },
   ]
 
+   const [likes, setLikes] = useState(Array(songs.length).fill(0));
+
+  const handleLike = () => {
+    const newLikes = [...likes];
+    newLikes[index] += 1;
+    setLikes(newLikes);
+  };
+
   return (
     <Wrapper>
-      {songs.map(song => (
-        <Song key={song.id} title={song.title} artist={song.artist} albumArt={song.albumArt} />
-      ))}
+      <Song
+        handleNext={handleNext}
+        handlePrev={handlePrev}
+        title={songs[index].title}
+        artist={songs[index].artist}
+        albumArt={songs[index].albumArt}
+        like={likes[index]}
+        onLike={handleLike}
+      />
     </Wrapper>
   )
 }
--- week02/JUNGAN/playlist2/src/Song.jsx
+++ week02/JUNGAN/playlist2/src/Song.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+// import { useState } from 'react'
+
+const ButtonWrapper = styled.div`
+  display: flex;    
+  flex-direction: row;
+  `;
 
 const Wrapper = styled.div`
   display: flex;
@@ -7,8 +13,8 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
   margin: 10px;
-  height: 300px;
-  width: 300px;
+  height: 400px;
+  width: 400px;
   border-radius: 15px;
   background-color: #f7eeda;
   color: #333;
@@ -16,16 +22,35 @@ const Wrapper = styled.div`
      &:hover {
         background-color: #e1ddd5;
     }
-    
+  img {
+    border-radius: 10px;
+    width: 250px;
+    height: 250px;
+  }
+  button {
+    position: absolute;
+    bottom: 300px;
+    left: 40%; 
+
+  }
 `;
 
 export default function Song(props) {
-    const { title, artist, albumArt } = props
+    const { title, artist, albumArt, handleNext, handlePrev, like, onLike} = props;
+    // const [like, setLike] = useState(0);
   return (
-    <Wrapper>
-        <img src={albumArt} alt={`${title} album art`} style={{ width: '150px', height: '150px' }} />
+    <ButtonWrapper>
+      <button onClick={handlePrev} style={{ margin: '5px' }}>Prev</button>
+      <Wrapper>
+        <button onClick={onLike} style={{ margin: '5px' }}>
+          {like} ❤️
+        </button>
+        <img src={albumArt} alt={`${title} album art`} />
         <h3>{title}</h3>
         <p style = {{margin: '1px'}}>{artist}</p>
     </Wrapper>
+      <button onClick={handleNext} style={{ margin: '5px' }}>Next</button>
+    </ButtonWrapper>
+    
   )
 }
